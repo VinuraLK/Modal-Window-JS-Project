a094f1962601759a991935eb756186a81a87124e
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2022-12-29 03:50:25 +0530

    added keydown event listener to close modal window when Esc key is pressed

--- script.js
+++ script.js
@@ -16,8 +16,16 @@ const openModal = function () {
 };
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
-
-  btnCloseModal.addEventListener("click", closeModal);
-
-  overlay.addEventListener("click", closeModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
