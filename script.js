f6c36f95b8c0147477d8bf8d419ed47873e74a1f
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2022-12-29 03:26:46 +0530

    Added functions to make the code more Dry

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
 
-  btnCloseModal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
+  btnCloseModal.addEventListener("click", closeModal);
 
-  overlay.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
+  overlay.addEventListener("click", closeModal);
 }
